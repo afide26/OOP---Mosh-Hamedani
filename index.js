a9f31ce43be5de16d0980741d763d299a8cd6238
Alan Fidelino <afidedev26@gmail.com>
2019-01-02 21:19:04 +1100

    Abstaction

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
-//Enumerating properties in an object
-
+//Abstraction
+//Hide the details; only show the essentials
 
 function Circle(radius) {
   this.radius = radius;
+  //Implementation details are stored in local variables and not assigned to this;
+  let defaultLocation = {
+    x: 0,
+    y: 0
+  };
+  let computeOptimumLocation = function () {
+    //...
+  };
   this.draw = function () {
+    computeOptimumLocation();
     console.log('draw');
-  }
+  };
 }
 
 const circle = new Circle(10);
 
-//Enumerate keys or values 'for in' loop
-for (let key in circle) {
-  //get the ones that are not functions
-  if (typeof circle[key] !== 'function')
-    console.log(key, circle[key]);
-}
-//Get all keys Object.keys
-const keys = Object.keys(circle);
-console.log('Keys', keys);
-
-circle.location = {
-  x: 1,
-  y: 2
-}
-// Find if a property is in an object using the 'in' method
-let prop = 'location';
-
-if (prop in circle) {
-  console.log(`Yes ${prop} is a property of circle`)
-} else {
-  console.log(`${prop} is not a property of circle.`)
-}
+//Closures - inner functions having access to internal local variables and local variables of parent functions.
