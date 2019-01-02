d79bef8df542b7910ee162d7db0b88c710b7aa15
Alan Fidelino <afidedev26@gmail.com>
2019-01-03 00:29:19 +1100

    Inheritance and Multi-level inheritance

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-//Exercise Stop Watch
-//duration; property - 0;
-// //reset, start, stop
-// function Stopwatch() {
-//   let startTime, endTime, running, duration = 0;
+//Lesson Inheritance and Multiple Level Inheritance
 
-//   this.start = function () {
-//     if (running)
-//       throw new Error('The stopwatch is already running');
-//     running = true;
-//     startTime = new Date();
-//     console.log(startTime.getTime()); //gets the seconds of the new Date instance;
-//   };
-//   this.stop = function () {
-//     if (!running)
-//       throw new Error('The stopwatch is not running.')
-//     running = false;
-//     endTime = new Date();
-//     let totalTime = (endTime.getTime() - startTime.getTime()) / 1000;
-//     return duration += totalTime;
-//   };
-//   this.reset = function () {
-//     startTime = null;
-//     endTime = null;
-//     duration = 0;
-//   };
+//Notes:
+// Classical Inheritance for languages with Classes. JS does not have classes;
+// Base/Super/Parent Class e.g. Shape
+// Derived/Sub/Child Class e.g. Square, Circle
+// Relationship IS-A: Square is a Shape, Circle is a Shape
 
-//   Object.defineProperty(this, 'duration', {
-//     get: function () {
-//       return duration;
-//     }
-//   })
-// }
+//Prototypical Inheritance
+//Parent of another object = Prototype; (parent) or ObjectBase or Template;
+let x = {} // Will have a __proto__ or Prototype of Object;
+let y = {} // Will have a __proto__ or Prototype of Object;
+//Use the Object.getPrototypeOf() method;
+
+Object.getPrototypeOf(x) === Object.getPrototypeOf(y);
+console.log(Object.getPrototypeOf(x) === Object.getPrototypeOf(y));
+
+//Inherit a method from the Proto or Prototypical inheritance;
+console.log(x.toString());
 
 
-// const sw = new Stopwatch();
+//Multi-level inheritance;
+
+function Circle() {
+  this.radius = 1;
+  this.draw = function () {
+    console.log('draw');
+  }
+}
+
+const circle = new Circle();
--- titles.js
+++ titles.js
@@ -3,45 +3,9 @@ window.onload = function () {
   let title = document.getElementById('title');
   let titles = [title, docTitle];
   for (let item of titles) {
-    item.innerHTML = 'Exercise: StopWatch';
+    item.innerHTML = 'Inheritance and Multiple Level Inheritance';
   }
 
   let para = document.getElementById('para1');
-  para.innerHTML = 'in-depth learning of Objects'
+  para.innerHTML = 're-using code'
 }
-
-function StopWatch() {
-  let startTime, endTime, running, duration = 0;
-
-  this.start = function () {
-    if (running)
-      throw new Error('The timer is already running');
-    running = true;
-    startTime = new Date();
-  }
-
-  this.stop = function () {
-    if (!running)
-      throw new Error('The timer has already stopped!')
-    running = false;
-    endTime = new Date();
-    let seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-    duration += seconds;
-  }
-
-  this.reset = function () {
-    startTime = null;
-    endTime = null;
-    duration = 0;
-  }
-
-  Object.defineProperty(this, 'duration', {
-    get: function () {
-      if (duration > 60)
-        return `Time lapsed is ${Math.round(duration/60)} minutes`;
-      return `Time lapsed is ${duration} seconds`;
-    }
-  })
-}
-
-const sw = new StopWatch();
