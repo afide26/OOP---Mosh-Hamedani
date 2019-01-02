4d80afce5c287361997dd0e9d0415bdb6cffa78e
Alan Fidelino <afidedev26@gmail.com>
2019-01-02 21:06:37 +1100

    Enumerating properties

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Adding and removing properties
+//Enumerating properties in an object
 
 
 function Circle(radius) {
@@ -9,12 +9,26 @@ function Circle(radius) {
 }
 
 const circle = new Circle(10);
-circle.location = {
-  x: 1
-};
 
-circle['location'] = {
-  x: 2
+//Enumerate keys or values 'for in' loop
+for (let key in circle) {
+  //get the ones that are not functions
+  if (typeof circle[key] !== 'function')
+    console.log(key, circle[key]);
 }
+//Get all keys Object.keys
+const keys = Object.keys(circle);
+console.log('Keys', keys);
 
-delete circle.location;
+circle.location = {
+  x: 1,
+  y: 2
+}
+// Find if a property is in an object using the 'in' method
+let prop = 'location';
+
+if (prop in circle) {
+  console.log(`Yes ${prop} is a property of circle`)
+} else {
+  console.log(`${prop} is not a property of circle.`)
+}
