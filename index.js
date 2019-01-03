0be096259628163d67807bd78dc05bb68f926d56
Alan Fidelino <afidedev26@gmail.com>
2019-01-03 18:11:00 +1100

    Inheritance vs Prototype Members

--- index.js
+++ index.js
@@ -3,14 +3,26 @@
 
 //Property descriptors
 
-//Constructor prototypes
+//Prototype vs Instance Members
 
-//Object.getPrototypeOf(circle);
 
-//result: myObj.__proto__(parent of myObj)
 
 function Circle(radius) {
-  this.radius = radius
+  //Instance member
+  this.radius = radius;
 }
 
-const circle = new Circle(1);
+//Transfer the draw method in the prototype of Circle to avoid duplication in memory
+
+//Prototype member
+Circle.prototype.draw = function () {
+  console.log('Draw')
+}
+const c1 = new Circle(1);
+const c2 = new Circle(10);
+
+//Existing methods of prototype can also be overriden
+
+Circle.prototype.toString = function () {
+  console.log(`Circle with a radius of ${this.radius}`);
+}
