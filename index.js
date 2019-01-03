ae171a45fc01f96329399ff7aaafc4606e694342
Alan Fidelino <afidedev26@gmail.com>
2019-01-03 20:11:28 +1100

    Polymorphism

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-//Method Overriding 
+//Polymorphism
+
+// This lesson shows the multiple forms of duplicate. 
+// Same method name but different implementations
 function extend(Child, Parent) {
   Child.prototype = Object.create(Parent.prototype);
   Child.prototype.constructor = Child;
@@ -20,7 +23,25 @@ extend(Circle, Shape);
 
 Circle.prototype.duplicate = function () {
   //to call the duplicate method from shape, use call
-  Shape.prototype.duplicate.call(this);
+  //Shape.prototype.duplicate.call(this);
   console.log('Duplicate Circle')
 }
-const c = new Circle()
+
+
+function Square() {
+
+}
+extend(Square, Shape);
+
+Square.prototype.duplicate = function () {
+  console.log('Duplicate Square')
+}
+
+//To test the power of polymorphism, see the examples below
+
+const shapes = [new Circle(), new Square()];
+
+//Iterate the shapes array using for of
+
+for (let shape of shapes)
+  shape.duplicate();
--- titles.js
+++ titles.js
@@ -3,7 +3,7 @@ window.onload = function () {
   let title = document.getElementById('title');
   let titles = [title, docTitle];
   for (let item of titles) {
-    item.innerHTML = 'Method Overriding';
+    item.innerHTML = 'Polymorphism';
   }
 
   let para = document.getElementById('para1');
