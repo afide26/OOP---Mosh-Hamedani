f7bfcbe25dfa88e08b5d9e7c807a085bf004b064
Alan Fidelino <afidedev26@gmail.com>
2019-01-04 17:54:36 +1100

    ES6 - CommonJS only runs in NodeJS

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-//Method Overriding
-
-class Shape {
-  move() {
-    console.log('Move')
-  }
-}
-
-//Inherit Shape for the Circle Class
-class Circle extends Shape {
-  move() {
-    //to call the move method from the parent object use super
-    super.move();
-    console.log('circle move')
-  }
-}
-
-const c = new Circle('blue', 20);
+//Common JS
+const Circle = require('./circle')
+const c = new Circle(10);
+c.draw();
--- titles.js
+++ titles.js
@@ -3,9 +3,9 @@ window.onload = function () {
   let title = document.getElementById('title');
   let titles = [title, docTitle];
   for (let item of titles) {
-    item.innerHTML = 'ES6 - Method Overriding';
+    item.innerHTML = 'ES6 - Modules';
   }
 
   let para = document.getElementById('para1');
-  para.innerHTML = 'More studies on the "this" keyword';
+  para.innerHTML = 'Common JS and ES6 Modules';
 }
