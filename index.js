a8873de3b6ae2d6a997afcf2cadc90056dff9495
Alan Fidelino <afidedev26@gmail.com>
2019-01-04 16:52:14 +1100

    ES6 - Method Overriding

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-//Inheritance
+//Method Overriding
 
 class Shape {
-  constructor(color) {
-    this.color = color;
-  }
   move() {
     console.log('Move')
   }
 }
 
 //Inherit Shape for the Circle Class
-
 class Circle extends Shape {
-  constructor(color, radius) {
-    super(color);
-    this.radius = radius;
-  }
-  draw() {
-    console.log('Draw');
+  move() {
+    //to call the move method from the parent object use super
+    super.move();
+    console.log('circle move')
   }
 }
 
 const c = new Circle('blue', 20);
-//** In this implementation, the Circle.prototype.constructor need not be declared. It will automatically have the Circle as constructor */
--- titles.js
+++ titles.js
@@ -3,7 +3,7 @@ window.onload = function () {
   let title = document.getElementById('title');
   let titles = [title, docTitle];
   for (let item of titles) {
-    item.innerHTML = 'ES6 - Inheritance';
+    item.innerHTML = 'ES6 - Method Overriding';
   }
 
   let para = document.getElementById('para1');
