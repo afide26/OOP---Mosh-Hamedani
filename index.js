e0cb09b7de8ff398ffca760a993664c52cf2bdc5
Alan Fidelino <afidedev26@gmail.com>
2019-01-04 16:40:14 +1100

    ES6 - Getters and Setters

--- index.js
+++ index.js
@@ -3,21 +3,20 @@
 //Indicate radius as a private property
 
 const _radius = new WeakMap();
-const _move = new WeakMap();
+
 class Circle {
   constructor(radius) {
     _radius.set(this, radius);
-
-    //set a private method
-    _move.set(this, () => {
-      console.log('move', this);
-    })
+  }
+  //ES6 getter
+  get radius() {
+    return _radius.get(this);
   }
 
-  //To access the weakmap with a public method
-  draw() {
-    _move.get(this)();
-    console.log('draw')
+  //ES6 Setter
+  set radius(value) {
+    if (typeof value !== 'number' || value <= 0) throw new Error('invalid radius');
+    _radius.set(this, value)
   }
 }
 
--- titles.js
+++ titles.js
@@ -3,7 +3,7 @@ window.onload = function () {
   let title = document.getElementById('title');
   let titles = [title, docTitle];
   for (let item of titles) {
-    item.innerHTML = 'ES6 - Private Properties Using Symbols';
+    item.innerHTML = 'ES6 - Getters and Setters';
   }
 
   let para = document.getElementById('para1');
